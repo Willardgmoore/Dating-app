4f6085173b1151dd5bd4127339634082d920ecd1
Will <will@thefixitgeek.com>
2015-09-25 17:10:48 -0600

    set up routes and correct nav

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,14 @@
+class LikesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def matches
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  resources :users
+  resources :likes 
+
+  get '/matches', to: 'likes#matches', as: :matches
+  get '/profile', to: 'users#profile', as: :profile
 
   devise_for :users
   
