3db5132c10b00fc5a079268e24aae2cd1091bac5
Will <will@thefixitgeek.com>
2015-09-26 16:59:52 -0600

    update views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,17 +4,41 @@ class UsersController < ApplicationController
   end
 
   def index
+  	@users = User.all#where(gender: true)#TODO where gender != current user
   end
 
   def show
+  	@user = User.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		redirect_to root_path
+  	else
+  		render :new
+  	end
   end
 
   def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
   end
 
   def destroy
-  	# @user = User.find(params)
-  	# @user.destroy
+  	@user = User.find(params)
+  	@user.destroy
+  	redirect_to users_path
   end
 
+  private
+
+  def user_params
+  	params.require(:user).permit(:first_name, :last_name, :age)
+  end
 end
