7b97eaa015ddd380db7ebfeec450f505d86f4808
Will <will@thefixitgeek.com>
2015-09-27 00:02:57 -0600

    WIP: suggested edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   :set_current_user
 
   
@@ -9,16 +10,17 @@ class ApplicationController < ActionController::Base
   	User.current = set_current_user
   end
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :first_name, :last_name, :age, :gender, :city, :state, :vehicle_type
-    devise_parameter_sanitizer.for(:account_up) << :first_name, :last_name, :age, :gender, :city, :state, :vehicle_type
+	devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:first_name, :last_name, :age, :gender, :city, :state, :vehicle_type) }
+	devise_parameter_sanitizer.for(:account_signup) { |u| u.permit(:first_name, :last_name, :age, :gender, :city, :state, :vehicle_type) }
+    # devise_parameter_sanitizer.for(:sign_up) << :first_name, :last_name, :age, :gender, :city, :state, :vehicle_type
+    # devise_parameter_sanitizer.for(:account_up) << :first_name, :last_name, :age, :gender, :city, :state, :vehicle_type
   end
   
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
