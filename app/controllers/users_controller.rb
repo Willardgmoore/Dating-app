75f4f2bbdd017cd89967afdbbeca44f53e9f6948
Will <will@thefixitgeek.com>
2015-09-26 18:54:48 -0600

    WIP: improve integration with devise, current_user, Update profile view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  
-  def profile
+  before_filter :set_current_user
 
+  def profile
+    @user = current_user # Replace current_user with something???!
   end
 
   def index
@@ -16,30 +17,34 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(user_params)
-  	if @user.save
-  		redirect_to root_path
-  	else
-  		render :new
-  	end
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
   end
 
   def destroy
-  	@user = User.find(params)
-  	@user.destroy
-  	redirect_to users_path
+    @user = User.find(params)
+    @user.destroy
+    redirect_to users_path
   end
 
   private
 
+  def set_current_user
+    User.current = current_user
+  end
+
   def user_params
-  	params.require(:user).permit(:first_name, :last_name, :age)
+    params.require(:user).permit(:first_name, :last_name, :age)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+  def self.current
+    Thread.current[:user]
+  end
+  def self.current=(user)
+    Thread.current[:user] = user
+  end
+User.current
+User.current=
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
