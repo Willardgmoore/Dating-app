02effc38488602e2ea3da2e97820016e03af5e78
Will <will@thefixitgeek.com>
2015-09-27 00:15:46 -0600

    WIP: profile update and set working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user,  :set_current_user
+  before_action :authenticate_user!,  :set_current_user
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   
   private
 
   def set_current_user
-  	User.current = set_current_user
+  	User.current = current_user
   end
 
   protected
 
   def configure_permitted_parameters
-	devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:first_name, :last_name, :age, :gender, :city, :state, :vehicle_type) }
-	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :age, :gender, :city, :state, :vehicle_type) }
+	devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:first_name, :last_name, :age, :gender, :city, :state, :vehicle_type,:password, :password_confirmation) }
+	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :age, :gender, :city, :state, :vehicle_type, :password, :password_confirmation, :current_password) }
     # devise_parameter_sanitizer.for(:sign_up) << :first_name, :last_name, :age, :gender, :city, :state, :vehicle_type
     # devise_parameter_sanitizer.for(:account_up) << :first_name, :last_name, :age, :gender, :city, :state, :vehicle_type
   end
