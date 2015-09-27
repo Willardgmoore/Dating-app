b411b3439e27db5167cfcf5326331c855fd1dfc1
Will <will@thefixitgeek.com>
2015-09-26 23:34:04 -0600

    WIP: suggested edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
   has_many :users
+  
   def self.current
-    self.current[:user]
+    Thread.current[:user]
   end
   def self.current=(user)
-    self.current[:user] = user
+    Thread.current[:user] = user
   end
-# User.current
-# User.current=
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
 end
