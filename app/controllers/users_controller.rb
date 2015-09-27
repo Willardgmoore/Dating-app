5c597160af51f97a28c26172c2ad093897f767a9
Will <will@thefixitgeek.com>
2015-09-26 19:48:44 -0600

    WIP: update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,12 +14,13 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to user_path # Was root path
     else
       render :new
     end
@@ -30,6 +31,13 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to users_path
+    else 
+      render :edit
+    end
   end
 
   def destroy
@@ -45,6 +53,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :age)
+    params.require(:user).permit(:first_name, :last_name, :age, :gender, :city, :state, :vehicle_type)
   end
 end
